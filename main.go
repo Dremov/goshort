8ac437a800c39fcd3abcbbc73e089c4d53f3c661
Dremov <andrew.dremov@gmail.com>
2025-12-06 19:03:24 +0100

    Add endpoints to shorten links and redirect

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
+	"net/http"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -13,6 +15,14 @@ type Link struct {
 	ID          int
 	OriginalUrl string
 	ShortCode   string
+}
+
+type ShortenRequest struct {
+	URL string `json:"url"`
+}
+
+type ShortenResponse struct {
+	ShortUrl string `json:"short_code"`
 }
 
 func main() {
@@ -32,26 +42,43 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var originalUrl = "https://google.com"
-	shortUrl, err := createShortLink(db, originalUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
+		// A. Parse the Short Code
+		// r.URL.Path returns the path like "/abc12"
+		// We slice it [1:] to remove the leading slash, leaving "abc12"
+		shortPath := request.URL.Path[1:]
 
-	fmt.Printf("Resulting URL is: %s\n", shortUrl)
+		// If the code is empty (user visited just "/"), just print a message
+		if shortPath == "" {
+			fmt.Fprintf(responseWriter, "Welcome to GoShort! Use the API to create links.")
+			return
+		}
 
-	retrievedUrl, err := getOriginalLink(db, shortUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
+		longPath, err := getOriginalLink(db, shortPath)
+		if err != nil {
+			http.Error(responseWriter, err.Error(), http.StatusNotFound)
+			return
+		}
 
-	fmt.Printf("Original URL was: %s\n", retrievedUrl)
+		// D. Redirect the User
+		//    If found, use this function to send them to the original URL:
+		//    http.Redirect(w, r, originalUrl, http.StatusFound)
+		http.Redirect(responseWriter, request, longPath, http.StatusFound)
+	})
+
+	http.HandleFunc("/shorten", func(responseWriter http.ResponseWriter, request *http.Request) {
+		// Paste the logic from step 2 here so it can see 'db'
+		handleShorten(responseWriter, request, db)
+	})
+
+	fmt.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func createShortLink(db *sql.DB, originalUrl string) (string, error) {
 	var id int
 	var shortUrl = generateShortCode()
-	var sqlQuery = `INSERT INTO links (original_url, short_code) 
+	var sqlQuery = `INSERT INTO links (original_url, short_code)
 								VALUES ($1, $2) 
 								RETURNING id;`
 
@@ -62,6 +89,43 @@ func createShortLink(db *sql.DB, originalUrl string) (string, error) {
 
 	fmt.Println("Created short link with ID: ", id)
 	return shortUrl, err
+}
+
+func handleShorten(responseWriter http.ResponseWriter, request *http.Request, db *sql.DB) {
+	// 1. Validate Method
+	if request.Method != http.MethodPost {
+		http.Error(responseWriter, "Only POST allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// 2. Parse JSON Body
+	var shortenRequest ShortenRequest
+	// We create a decoder that reads from the Request Body and decodes into our struct
+	if err := json.NewDecoder(request.Body).Decode(&shortenRequest); err != nil {
+		http.Error(responseWriter, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	// --- YOUR CODE HERE ---
+
+	// 3. Call your createShortLink function using req.URL
+	//    (Handle the error if it fails)
+	shortUrl, err := createShortLink(db, shortenRequest.URL)
+	if err != nil {
+		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// 4. Send JSON Response
+	//    A. Create an instance of ShortenResponse with the new code
+	//    B. Set Header: w.Header().Set("Content-Type", "application/json")
+	//    C. Encode it: json.NewEncoder(w).Encode(response)
+	shortenResponse := ShortenResponse{
+		ShortUrl: shortUrl,
+	}
+
+	responseWriter.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(responseWriter).Encode(shortenResponse)
 }
 
 func getOriginalLink(db *sql.DB, shortUrl string) (string, error) {
